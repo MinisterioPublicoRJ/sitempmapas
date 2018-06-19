341ae29038a3290f656b2918e4ba72c1a55ddaf4
Arlindo Pereira <arlindo.pereira@mprj.mp.br>
2018-06-19 20:23:17 -0300

    Fixing favicon task

--- tasks/taskBuildFavicon.js
+++ tasks/taskBuildFavicon.js
@@ -1,6 +1,6 @@
 // Inicia diretorio raiz e copia favicons
 module.exports = function (gulp, plugins, config) {
-    gulp.task("buildFavico", ["clean"], function(){
+    gulp.task("buildFavicon", ["clean"], function(){
         return gulp
             .src(config.favIcoSrcPath)
             .pipe(gulp.dest(config.favIcoSrcDist));
